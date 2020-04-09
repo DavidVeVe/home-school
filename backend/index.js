e838aff18e90eee2c33d69f8647a906b7b32d168
Khay <altaircarn@gmail.com>
2020-04-09 15:34:01 -0500

    [ADD] backend login and register

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const app = express();
 const authRoute = require('./routes/auth');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
+const app = express();
+// Configurations
+app.set('PORT', 3000 || process.env.PORT);
+const PORT = app.get('PORT');
+
+// Database Configuration
+const urlDatabase = process.env.DB_URL;
+mongoose.connect(
+	urlDatabase,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log('Database Running')
+);
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.use('/', authRoute);
 
-app.listen(3000, () => {
+// App Listen
+app.listen(PORT, () => {
 	console.log('Hello guys, the app is working!');
 });
