d3db06d2cb00b22c2f1d32d9adebecd27a18d8e3
Jose Perez <gpe9503@hotmail.com>
2020-04-10 16:08:13 -0500

    merge

--- frontend/src/containers/HomePage/HomePage.js
+++ frontend/src/containers/HomePage/HomePage.js
@@ -8,46 +8,27 @@ import TeacherPage from '../Teacher/Teacher';
 import HomeAvatar from './homeAvatar/homeAvatar';
 
 const HomePage = (props) => {
-<<<<<<< HEAD
   return (
-    <div className='homepage'>
+    <div>
       <main>
         {!props.token ? (
-          <Fragment>
-            <h2>LOGO</h2>
+          <section className='homePage'>
+            <div className='homeAvatar'>
+              <h1>Inicio de sesión recientes</h1>
+              <section className='homeAvatar__container'>
+                <HomeAvatar />
+                <HomeAvatar />
+                <HomeAvatar />
+              </section>
+            </div>
             <SignUp />
-          </Fragment>
+          </section>
         ) : (
           <TeacherPage />
         )}
       </main>
-      <About />
-      <Footer />
     </div>
   );
-=======
-	return (
-		<div>
-			<main>
-				{!props.token ? (
-					<section className='homePage'>
-						<div className='homeAvatar'>
-							<h1>Inicio de sesión recientes</h1>
-							<section className='homeAvatar__container'>
-								<HomeAvatar />
-								<HomeAvatar />
-								<HomeAvatar />
-							</section>
-						</div>
-						<SignUp />
-					</section>
-				) : (
-					<TeacherPage />
-				)}
-			</main>
-		</div>
-	);
->>>>>>> 7c75087367a4562a59a68aba1d958305706120c7
 };
 
 const mapStateToProps = (state) => ({
